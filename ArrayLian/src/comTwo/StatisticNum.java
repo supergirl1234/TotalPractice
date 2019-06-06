1b632e608bc4f9a814704bfaf51be1432b7d4d87
范蕾蕾 <2547462712@qq.com>
2019-06-06 23:40:20 +0800

    给定一个k位整数N = dk-1*10k-1 + ... + d1*101 + d0 (0<=di<=9, i=0,...,k-1,dk-1>0 )，请编写程序统计每种不同的个位数字出现的次数。例如：给定N=100311,则有2个0，3个1，1个3

---
+++ ArrayLian/src/comTwo/StatisticNum.java
@@ -0,0 +1,16 @@
+package comTwo;
+
+/**
+ * Author:Fanleilei
+ * Created:2019/6/5 0005
+ *
+ *
+ * 给定一个k位整数N = dk-1*10k-1 + ... + d1*101 + d0 (0<=di<=9, i=0,...,k-1, dk-1>0)，请编写程序统计每种不同的个位数字出现的次数。例如：给定N = 100311，则有2个0，3个1，和1个3。
+ */
+public class StatisticNum {
+
+    public static void main(String[] args) {
+
+
+    }
+}
